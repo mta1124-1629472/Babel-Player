7c20eaf9ca7cf0e2d05be4cfe0cf266aedd2fc01
mta-babel <babelworks.player@gmail.com>
2026-04-17 15:58:11 -0700

    fix(ui): resolve shortcut and accessibility review comments

---
+++ Views/MainWindowShortcutRouter.cs
@@ -0,0 +1,70 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.VisualTree;
+
+namespace Babel.Player.Views;
+
+internal enum MainWindowShortcutAction
+{
+    None = 0,
+    PlayPause,
+    ToggleSubtitles,
+    ToggleSegmentPane,
+    ToggleDubMode,
+    ToggleFullscreen,
+    ExitFullscreen,
+}
+
+internal static class MainWindowShortcutRouter
+{
+    public static bool TryMap(
+        Key key,
+        KeyModifiers modifiers,
+        IInputElement? focusedElement,
+        bool isFullscreen,
+        out MainWindowShortcutAction action)
+    {
+        action = MainWindowShortcutAction.None;
+
+        if (modifiers != KeyModifiers.None)
+            return false;
+
+        if (key == Key.Escape && isFullscreen)
+        {
+            action = MainWindowShortcutAction.ExitFullscreen;
+            return true;
+        }
+
+        if (key == Key.F11)
+        {
+            action = MainWindowShortcutAction.ToggleFullscreen;
+            return true;
+        }
+
+        if (ShouldSuppressCharacterShortcut(focusedElement))
+            return false;
+
+        action = key switch
+        {
+            Key.Space => MainWindowShortcutAction.PlayPause,
+            Key.C => MainWindowShortcutAction.ToggleSubtitles,
+            Key.S => MainWindowShortcutAction.ToggleSegmentPane,
+            Key.D => MainWindowShortcutAction.ToggleDubMode,
+            _ => MainWindowShortcutAction.None,
+        };
+
+        return action != MainWindowShortcutAction.None;
+    }
+
+    private static bool ShouldSuppressCharacterShortcut(IInputElement? focusedElement)
+    {
+        for (var visual = focusedElement as Visual; visual is not null; visual = visual.GetVisualParent())
+        {
+            if (visual is TextBox or ComboBox)
+                return true;
+        }
+
+        return false;
+    }
+}
